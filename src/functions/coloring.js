37a1271398fe0eb7022577236d61ee5a38ade614
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2022-01-12 23:01:26 -0500

    Removed functions folder. Functions should be in class components per assignment description

--- src/functions/coloring.js
+++
@@ -1,9 +0,0 @@
-/*
- * This file contains the following functions
- * setColor()
- * setCellColor()
- * colorAll()
- * resetColors()
- * dragAndDrop()
- * 
-*/
--- src/functions/column.js
+++
@@ -1,6 +0,0 @@
-/*
- * This file contains the following functions
- * addCol()
- * delCol()
- * 
-*/
